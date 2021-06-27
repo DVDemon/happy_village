826f02baca9f95d0586a61cfd237f33f786fd873
Dmitriy Dzyuba <ddzuba@yandex.ru>
2021-06-27 14:42:42 +0300

    second commit

---
+++ database/person.cpp
@@ -0,0 +1,341 @@
+#include "person.h"
+#include "database.h"
+#include "cache.h"
+
+#include "../config/config.h"
+#include <Poco/Data/MySQL/Connector.h>
+#include <Poco/Data/MySQL/MySQLException.h>
+#include <Poco/Data/SessionFactory.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/Dynamic/Var.h>
+#include <cppkafka/cppkafka.h>
+
+#include <sstream>
+#include <exception>
+
+using namespace Poco::Data::Keywords;
+using Poco::Data::Session;
+using Poco::Data::Statement;
+
+namespace database
+{
+
+    void Person::init()
+    {
+        try
+        {
+
+            Poco::Data::Session session = database::Database::get().create_session_write();
+            //*
+            Statement drop_stmt(session);
+            drop_stmt << "DROP TABLE IF EXISTS Person", now;
+            //*/
+
+            // (re)create table
+            Statement create_stmt(session);
+            create_stmt << "CREATE TABLE IF NOT EXISTS `Person` (`id` INT NOT NULL AUTO_INCREMENT,"
+                        << "`first_name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,"
+                        << "`last_name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,"
+                        << "`email` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,"
+                        << "`title` VARCHAR(1024) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,"
+                        << "PRIMARY KEY (`id`),KEY `fn` (`first_name`),KEY `ln` (`last_name`));",
+                now;
+        }
+
+        catch (Poco::Data::MySQL::ConnectionException &e)
+        {
+            std::cout << "connection:" << e.what() << std::endl;
+            throw;
+        }
+        catch (Poco::Data::MySQL::StatementException &e)
+        {
+
+            std::cout << "statement:" << e.what() << std::endl;
+            throw;
+        }
+    }
+
+    Poco::JSON::Object::Ptr Person::toJSON() const
+    {
+        Poco::JSON::Object::Ptr root = new Poco::JSON::Object();
+
+        root->set("id", _id);
+        root->set("first_name", _first_name);
+        root->set("last_name", _last_name);
+        root->set("email", _email);
+        root->set("title", _title);
+
+        return root;
+    }
+
+    Person Person::fromJSON(const std::string &str)
+    {
+        Person person;
+        Poco::JSON::Parser parser;
+        Poco::Dynamic::Var result = parser.parse(str);
+        Poco::JSON::Object::Ptr object = result.extract<Poco::JSON::Object::Ptr>();
+
+        person.id() = object->getValue<long>("id");
+        person.first_name() = object->getValue<std::string>("first_name");
+        person.last_name() = object->getValue<std::string>("last_name");
+        person.email() = object->getValue<std::string>("email");
+        person.title() = object->getValue<std::string>("title");
+
+        return person;
+    }
+
+    Person Person::read_by_id(long id)
+    {
+        try
+        {
+            Poco::Data::Session session = database::Database::get().create_session_read();
+            Poco::Data::Statement select(session);
+            Person a;
+            select << "SELECT id, first_name, last_name, email, title FROM Person where id=?",
+                into(a._id),
+                into(a._first_name),
+                into(a._last_name),
+                into(a._email),
+                into(a._title),
+                use(id),
+                range(0, 1); //  iterate over result set one row at a time
+
+            if (!select.done())
+            {
+                select.execute();
+            }
+
+            return a;
+        }
+
+        catch (Poco::Data::MySQL::ConnectionException &e)
+        {
+            std::cout << "connection:" << e.what() << std::endl;
+            throw;
+        }
+        catch (Poco::Data::MySQL::StatementException &e)
+        {
+
+            std::cout << "statement:" << e.what() << std::endl;
+            throw;
+        }
+    }
+
+    Person Person::read_from_cache_by_id(long id)
+    {
+
+        try
+        {
+            std::string result;
+            if (database::Cache::get().get(id, result))
+                return fromJSON(result);
+            else
+                throw std::logic_error("key not found in the cache");
+        }
+        catch (std::exception err)
+        {
+            std::cout << "error:" << err.what() << std::endl;
+            throw;
+        }
+    }
+
+    void Person::warm_up_cache()
+    {
+        std::cout << "wharming up authors cache ...";
+        auto array = read_all();
+        long count = 0;
+        for (auto &a : array)
+        {
+            a.save_to_cache();
+            ++count;
+        }
+        std::cout << "done: " << count << std::endl;
+    }
+
+    std::vector<Person> Person::read_all()
+    {
+        try
+        {
+            Poco::Data::Session session = database::Database::get().create_session_read();
+            Statement select(session);
+            std::vector<Person> result;
+            Person a;
+            select << "SELECT id, first_name, last_name, email, title FROM Person",
+                into(a._id),
+                into(a._first_name),
+                into(a._last_name),
+                into(a._email),
+                into(a._title),
+                range(0, 1); //  iterate over result set one row at a time
+
+            while (!select.done())
+            {
+                select.execute();
+                result.push_back(a);
+            }
+            return result;
+        }
+
+        catch (Poco::Data::MySQL::ConnectionException &e)
+        {
+            std::cout << "connection:" << e.what() << std::endl;
+            throw;
+        }
+        catch (Poco::Data::MySQL::StatementException &e)
+        {
+
+            std::cout << "statement:" << e.what() << std::endl;
+            throw;
+        }
+    }
+
+    std::vector<Person> Person::search(std::string first_name, std::string last_name)
+    {
+        try
+        {
+            Poco::Data::Session session = database::Database::get().create_session_read();
+            Statement select(session);
+            std::vector<Person> result;
+            Person a;
+            select << "SELECT id, first_name, last_name, email, title FROM Person where first_name LIKE ? and last_name LIKE ?",
+                into(a._id),
+                into(a._first_name),
+                into(a._last_name),
+                into(a._email),
+                into(a._title),
+                use(first_name),
+                use(last_name),
+                range(0, 1); //  iterate over result set one row at a time
+
+            while (!select.done())
+            {
+                select.execute();
+                result.push_back(a);
+            }
+            return result;
+        }
+
+        catch (Poco::Data::MySQL::ConnectionException &e)
+        {
+            std::cout << "connection:" << e.what() << std::endl;
+            throw;
+        }
+        catch (Poco::Data::MySQL::StatementException &e)
+        {
+
+            std::cout << "statement:" << e.what() << std::endl;
+            throw;
+        }
+    }
+
+    void Person::send_to_queue()
+    {
+        cppkafka::Configuration config = {
+            {"metadata.broker.list", Config::get().get_queue_host()}};
+
+        cppkafka::Producer producer(config);
+        std::stringstream ss;
+        Poco::JSON::Stringifier::stringify(toJSON(), ss);
+        std::string message = ss.str();
+        producer.produce(cppkafka::MessageBuilder(Config::get().get_queue_topic()).partition(0).payload(message));
+        producer.flush();
+    }
+
+    void Person::save_to_cache()
+    {
+        std::stringstream ss;
+        Poco::JSON::Stringifier::stringify(toJSON(), ss);
+        std::string message = ss.str();
+        database::Cache::get().put(_id, message);
+    }
+    void Person::save_to_mysql()
+    {
+
+        try
+        {
+            Poco::Data::Session session = database::Database::get().create_session_write();
+            Poco::Data::Statement insert(session);
+
+            insert << "INSERT INTO Person (first_name,last_name,email,title) VALUES(?, ?, ?, ?)",
+                use(_first_name),
+                use(_last_name),
+                use(_email),
+                use(_title),
+                now;
+
+            insert.execute();
+
+            Poco::Data::Statement select(session);
+            select << "SELECT LAST_INSERT_ID()",
+                into(_id),
+                range(0, 1); //  iterate over result set one row at a time
+
+            if (!select.done())
+            {
+                select.execute();
+            }
+            std::cout << "inserted:" << _id << std::endl;
+        }
+        catch (Poco::Data::MySQL::ConnectionException &e)
+        {
+            std::cout << "connection:" << e.what() << std::endl;
+            throw;
+        }
+        catch (Poco::Data::MySQL::StatementException &e)
+        {
+
+            std::cout << "statement:" << e.what() << std::endl;
+            throw;
+        }
+    }
+
+    long Person::get_id() const
+    {
+        return _id;
+    }
+
+    const std::string &Person::get_first_name() const
+    {
+        return _first_name;
+    }
+
+    const std::string &Person::get_last_name() const
+    {
+        return _last_name;
+    }
+
+    const std::string &Person::get_email() const
+    {
+        return _email;
+    }
+
+    const std::string &Person::get_title() const
+    {
+        return _title;
+    }
+
+    long &Person::id()
+    {
+        return _id;
+    }
+
+    std::string &Person::first_name()
+    {
+        return _first_name;
+    }
+
+    std::string &Person::last_name()
+    {
+        return _last_name;
+    }
+
+    std::string &Person::email()
+    {
+        return _email;
+    }
+
+    std::string &Person::title()
+    {
+        return _title;
+    }
+}
---
+++ database/person.h
@@ -0,0 +1,49 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+#include <vector>
+#include "Poco/JSON/Object.h"
+
+namespace database
+{
+    class Person{
+        private:
+            long _id;
+            std::string _first_name;
+            std::string _last_name;
+            std::string _email;
+            std::string _title;
+
+        public:
+
+            static Person fromJSON(const std::string & str);
+
+            long             get_id() const;
+            const std::string &get_first_name() const;
+            const std::string &get_last_name() const;
+            const std::string &get_email() const;
+            const std::string &get_title() const;
+
+            long&        id();
+            std::string &first_name();
+            std::string &last_name();
+            std::string &email();
+            std::string &title();
+
+            static void init();
+            static void warm_up_cache();
+            static Person read_by_id(long id);
+            static Person read_from_cache_by_id(long id);
+            static std::vector<Person> read_all();
+            static std::vector<Person> search(std::string first_name,std::string last_name);
+            void save_to_mysql();
+            void send_to_queue();
+            void save_to_cache();
+
+            Poco::JSON::Object::Ptr toJSON() const;
+
+    };
+}
+
+#endif
