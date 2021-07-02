fc1b94368cbf5f028ef51726ef41d12a5eba2bd5
Dmitriy Dzyuba <ddzuba@yandex.ru>
2021-07-02 22:57:52 +0300

    web server

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+#include "web_server/http_web_server.h"
 
-auto main() -> int {
-    return 1;
+
+int main(int argc, char*argv[]) 
+{
+    HTTPWebServer app;
+    return app.run(argc, argv);
 }
